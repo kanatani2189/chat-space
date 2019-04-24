7e846cebcb8d59a60cfca9cb73a1536b73d4f235
kanatani2189 <50.kanatani.hiromi@gmail.com>
2019-04-24 14:59:26 +0900

    Edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,5 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
-  def index
-    @user= current_user       
-  end
   
   def new
     @group = Group.new
